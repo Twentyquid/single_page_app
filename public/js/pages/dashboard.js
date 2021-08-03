3998510a3ce9e5929261ce8d6c2294c44c598105
Twentyquid <tardigrade0801@gmail.com>
2021-08-03 19:12:32 +0530

    started doing css added scss for styling still learnin sass

--- public/js/pages/dashboard.js
+++ public/js/pages/dashboard.js
@@ -7,7 +7,7 @@ export default class extends mainpage {
     }
     async getHtml(){
         return `
-        <h1>Welcome back Dom</h1>
+        <h2>Welcome back Dom</h1>
         <p>something goes here that tells about the dashboard</p>
         <p>I should have better used lorem ipsum</p>
         <p> But idon't know why i don't want to do that</p>
